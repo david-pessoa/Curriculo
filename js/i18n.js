06a832acf56e7ab9583c2003d9a9fbac2eb44ea5
david-pessoa <pessoa.dvlbp@gmail.com>
2025-12-10 00:43:08 -0300

    add currículo atualizado e add tradução com i18n parcial

---
+++ js/i18n.js
@@ -0,0 +1,31 @@
+async function i18nextInit() {
+  await i18next
+    .use(i18nextHttpBackend)
+    .use(i18nextBrowserLanguageDetector)
+    .init({
+      fallbackLng: "en", // se não achar, usa inglês
+      supportedLngs: ["en", "pt-BR"], // línguas suportadas
+      ns: ["translation"],
+      defaultNS: "translation",
+      backend: {
+        loadPath: "../locales/{{ lng }}/translation.json",
+      },
+      debug: true,
+    });
+  updateContent();
+}
+
+function updateContent() {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((el) => {
+    const key = el.getAttribute("data-i18n");
+    el.innerHTML = i18next.t(key);
+  });
+}
+
+async function changeLanguage(lng) {
+  await i18next.changeLanguage(lng);
+  updateContent();
+}
+
+i18nextInit();
