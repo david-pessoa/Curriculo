fd5dfa6810382867826377dd44a4e742f6956725
david-pessoa <pessoa.dvlbp@gmail.com>
2026-05-05 15:16:06 -0300

    passa info dos certificados para JSON

--- js/projects-list.js
+++ js/projects-list.js
@@ -7,20 +7,22 @@ export async function loadProjectsList() {
 	fetch('./dados.json')
 		.then((response) => {
 			if (!response.ok) {
-				throw new Error('Erro ao carregar o JSON: ' + response.status);
+				throw new Error(
+					'Erro ao carregar o JSON com projetos: ' + response.status
+				);
 			}
 			return response.json();
 		})
 		.then((data) => {
 			// Carrega boxes com os projetos dinamicamente
-			const projects = data;
+			const projects = data.projetos;
 			projects.map((project, i) => {
 				const item = document.createElement('li');
 				item.className = 'folio-item';
 				item.innerHTML = `
           <a href=/projetos/?id=${i + 1}>
             <figure>
-              <img src=${project.imagemHome} alt="${project.nome}" />
+              <img src="${project.imagemHome}" alt="${project.nome}" />
               <figcaption>
                 <h3>${i18next.t(`portfolio.projects.${i}.title`)}</h3>
                 <p>${i18next.t(`portfolio.projects.${i}.mini-description`)}</p>
@@ -29,6 +31,48 @@ export async function loadProjectsList() {
           </a>
         `;
 				projectList.appendChild(item);
+			});
+		})
+		.catch((error) => console.error(error));
+}
+
+export async function loadCertificatesList() {
+	const certificatesList = document.getElementById('owl-slider');
+	if (!certificatesList) return;
+
+	certificatesList.innerHTML = '';
+
+	fetch('./dados.json')
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(
+					'Erro ao carregar o JSON com certificados: ' + response.status
+				);
+			}
+			return response.json();
+		})
+		.then((data) => {
+			// Carrega boxes com os projetos dinamicamente
+			const certificates = data;
+			certificates.map((project, i) => {
+				const item = document.createElement('div');
+				item.className = 'service';
+				item.innerHTML = `
+          <h3 data-i18n=${i18next.t(`portfolio.projects.${i}.title`)}>
+							Certificado do curso de Introdução à Inteligência Artificial Generativa
+						</h3>
+						<a
+							class="icon"
+							href="https://www.skills.google/public_profiles/6c9bab69-6090-458a-bf56-de1248b18c8e/badges/23751369"
+							target="_blank"
+						>
+							<img
+								src="images/certificados/Introduction-to-Gen-AI.png"
+								alt="Badge do curso de Introdução a Gen AI"
+							/>
+						</a>
+        `;
+				certificatesList.appendChild(item);
 			});
 		})
 		.catch((error) => console.error(error));
